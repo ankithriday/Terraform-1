a8127daff3977763d8d939ce19997d6f59bef441
ankithriday <36642123+ankithriday@users.noreply.github.com>
2019-05-17 17:12:43 -0400

    Update main.tf

--- east/main.tf
+++ east/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web1" {
   instance_type = "${var.instance_type}"
   # key_name = "${var.key_name}"
   security_groups = ["${var.security_groups}"]
-  tags {
-    Name = "Instance1"
-  }
- }
+ # tags {
+  #  Name = "Instance1"
+#  }
+# }
