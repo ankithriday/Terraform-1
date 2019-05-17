f9b5fc66950143fcda9aa54dcebc23e3e78103e8
ankithriday <36642123+ankithriday@users.noreply.github.com>
2019-05-17 16:41:20 -0400

    Update main.tf

--- east/main.tf
+++ east/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web1" {
   ami = "${var.ami_image}"
   instance_type = "${var.instance_type}"
-  key_name = "${var.key_name}"
+  # key_name = "${var.key_name}"
   security_groups = ["${var.security_groups}"]
   tags {
     Name = "Instance1"
