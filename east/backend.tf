a43a2cbc53139146c56e84a4eec577dbfcda3eb6
ankithriday <36642123+ankithriday@users.noreply.github.com>
2019-05-17 17:01:45 -0400

    Update backend.tf

--- east/backend.tf
+++ east/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ec2jobprocessing05"
+    #bucket = "ec2jobprocessing05"
+    bucket = "ec2jobprocessing12"
     key    = "terraform/dev"
     region = "us-east-1"
   }
